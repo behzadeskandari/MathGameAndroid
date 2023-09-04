8ed66a13d4c2a07c5440902baba3fe8df767cf35
behzad <behzad.b.i.g@gmail.com>
2023-09-04 20:01:16 +0330

    end of Game Activity still colors needs works or better backgroind picture is needed

--- app/src/main/java/com/example/mathgame/game.java
+++ app/src/main/java/com/example/mathgame/game.java
@@ -2,11 +2,13 @@ package com.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class game extends AppCompatActivity {
@@ -28,9 +30,15 @@ public class game extends AppCompatActivity {
 
     int userAnswer;
     int realAnswer;
+    int answerTrue;
     int userScore = 0;
 
     int userLife = 3;
+
+    CountDownTimer timer ;
+    private static  final long START_TIMER_IN_MILES = 10000;
+    Boolean timer_running;
+    long time_left_in_miles = START_TIMER_IN_MILES;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +59,7 @@ public class game extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                     userAnswer = Integer.parseInt(answer.getText().toString());
+                    pauseTimer();
                     if (userAnswer == realAnswer){
                         userScore = userScore + 10;
                         question.setText("Congrats your Answer Is True");
@@ -66,6 +75,9 @@ public class game extends AppCompatActivity {
         nextQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                answer.setText("");
+                gameCountinue();
+                resetTimer();
 
             }
         });
@@ -77,7 +89,50 @@ public class game extends AppCompatActivity {
             number2 = random.nextInt(100);
             realAnswer = number1 + number2;
             question.setText(number1 +" + "+ number2);
+            ///answerTrue = number1 + number2;
+            ///startTimer();
+            ///ok.setClickable(true);
+
+            startTimer();
+    }
+
+    private void startTimer(){
+        timer = new CountDownTimer(time_left_in_miles,100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time_left_in_miles = millisUntilFinished;
+                updateText();
+            }
+
+            @Override
+            public void onFinish() {
+                timer_running = false;
+                pauseTimer();
+                resetTimer();
+                updateText();
+                userLife = userLife - 1;
+                question.setText("Sorry! Time is Up!");
+            }
+        }.start();
+
+        timer_running = true;
     }
 
 
+    public void updateText(){
+        int second = (int)(time_left_in_miles / 1000)%60;
+        String time_left = String.format(Locale.getDefault(),"%02d",second);
+        time.setText(time_left);
+    }
+
+    public void pauseTimer(){
+        timer.cancel();
+        timer_running = false;
+    }
+
+    public  void  resetTimer()
+    {
+        time_left_in_miles = START_TIMER_IN_MILES;
+        updateText();
+    }
 }
